850d796b956dc0375cc07c248d52d49c421f193b
XuGuohui <xghui999@126.com>
2020-12-10 16:33:00 +0800

    Gen2: reliable isOff()

--- hal/src/electron/modem/mdm_hal.cpp
+++ hal/src/electron/modem/mdm_hal.cpp
@@ -1246,7 +1246,7 @@ int MDMParser::getModemStateChangeCount(void) const {
     return _mdm_state_change_count;
 }
 
-bool MDMParser::powerState(void) const {
+bool MDMParser::powerState(void) {
     auto state = HAL_GPIO_Read(RI_UC);
     if (state) {
         // RI is high, which means that V_INT is high as well
@@ -1267,6 +1267,10 @@ bool MDMParser::powerState(void) const {
     state = HAL_GPIO_Read(RI_UC);
     // If RI is still low - we are off
     // If RI is high - we are on
+    if (getModemStateChangeCount() == 0) {
+        _incModemStateChangeCount();
+        _pwr = state;
+    }
     return state;
 }
 
